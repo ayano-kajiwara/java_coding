5a8057dd52139ff9a0ad7b1845072e975f01d3e1
ayano-kajiwara <aquamarine320a@gmail.com>
2022-12-27 10:46:30 +0900

    設問10を追加

---
+++ basis/Question10/Question10.java
@@ -0,0 +1,43 @@
+package basis.Question10;
+
+import java.util.Scanner;
+
+public class Question10 {
+	// 後に入力する整数を入れる変数
+	static int num;
+
+	public static void main(String[] args) {
+		// try-with-resouce
+		try (Scanner scan = new Scanner(System.in);) {
+			// メッセージ出力
+			System.out.println("整数を入力してください");
+			// キーボードから文字列入力
+			String inputStr = scan.nextLine();
+			// 入力文字列を整数へ変換
+			num = Integer.parseInt(inputStr);
+			//引数をnumとして、printCalculateDoubleメソッドを呼び出す
+			printCalculateDouble(num);
+			//引数をnumとして、printCalculateSquareメソッドを呼び出す
+			printCalculateSquare(num);
+
+			// 整数以外の値が入力された場合
+		} catch (NumberFormatException e) {
+			System.out.println("整数以外の値が入力されました");
+		}
+	}
+
+	// 引数を2倍にした値を出力する
+	public static void printCalculateDouble(int x) {
+		final int MULTIPLE = 2;
+		int doubleNum = x * MULTIPLE;
+		System.out.println("入力された　" + num + "　を" + MULTIPLE + "倍した値は　" + doubleNum + "　です");
+	}
+
+	// 引数を2乗にした結果を出力する
+	public static void printCalculateSquare(int x) {
+		final int POWER = 2;
+		int squareNum = (int) Math.pow(x, POWER);
+		System.out.println("入力された　" + num + "　を" + POWER + "乘した値は　" + squareNum + "　です");
+	}
+
+}
